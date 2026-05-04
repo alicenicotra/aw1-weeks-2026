3346d6c8411a69dfc71546aa330ad7ceb6f1f83a
alicenicotra <s361094@studenti.polito.it>
2026-05-04 15:43:59 +0200

    Esercizio 1 week10 completato

--- week10/exercise1/react-qa/src/components/NavHeader.jsx
+++ week10/exercise1/react-qa/src/components/NavHeader.jsx
@@ -1,11 +1,25 @@
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Navbar, Button } from "react-bootstrap";
 import { Link } from "react-router";
 
+import { useState, useEffect } from "react";
+
 function NavHeader (props) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if(darkMode)
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+    else
+      document.documentElement.removeAttribute("data-bs-theme");
+  }, [darkMode]);
+
   return(
     <Navbar bg="primary" data-bs-theme="dark">
       <Container fluid>
         <Link to="/" className="navbar-brand">HeapOverrun</Link>
+        <Button onClick={() => setDarkMode(oldMode => !oldMode)}> 
+          {darkMode ? <i className=" bi bi-sun-fill"></i> : <i className=" bi bi-moon-fill"></i>}
+        </Button>
       </Container>
     </Navbar>
   );
